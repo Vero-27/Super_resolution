0ef2e4bddd2dcbcf5aa03616610e6702a0870e8b
verob <veronique.bodin@etu.unlim.fr>
2025-10-14 16:33:23 +0200

    add .exe building

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
+import sys
+import os
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivy.core.window import Window
 
-Builder.load_file('GUI/Screens/styles.kv')
-Builder.load_file('GUI/Screens/home.kv')
-Builder.load_file('GUI/Screens/select.kv')
-Builder.load_file('GUI/Screens/result.kv')
+def resource_path(relative_path):
+    #Pour avoir le chemin absolu vers les différentes ressources, nécessaire pour le .exe et .apk
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+Builder.load_file(resource_path('GUI/Screens/styles.kv'))
+Builder.load_file(resource_path('GUI/Screens/home.kv'))
+Builder.load_file(resource_path('GUI/Screens/select.kv'))
+Builder.load_file(resource_path('GUI/Screens/result.kv'))
 
 from GUI.Managers.home import HomeScreen
 from GUI.Managers.select_screen import SelectScreen
 from GUI.Managers.result_screen import ResultScreen
 from Core.processing import run_super_resolution
-
 
 Window.size = (800, 600)
 
